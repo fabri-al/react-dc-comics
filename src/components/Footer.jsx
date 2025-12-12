2c48e590aac6bfa9de4c995754ca5b534cbca866
MyUsername <development.for.code@gmail.com>
2025-12-12 17:08:37 +0100

    react-dc-comics

---
+++ src/components/Footer.jsx
@@ -0,0 +1,95 @@
+function Footer() {
+
+    return (
+
+        <div id>
+
+            <div id="nav-mid-bottom">
+
+
+                <div class="navbar-mid-bottom">
+
+                    <a class="nav-link-bottom" href="#">Digital Comics</a>
+                    <a class="nav-link-bottom" href="#">DC Merchandise</a>
+                    <a class="nav-link-bottom" href="#">Subscription</a>
+                    <a class="nav-link-bottom" href="#">Comic Shop Locator</a>
+                    <a class="nav-link-bottom" href="#">DC Power Visa</a>
+
+
+                </div>
+                <div id="dc-comics">
+                    <h3 id="dc-comics-h3">DC COMICS</h3>
+
+                    <div class="footer-links-dcComics">
+                        <a class="links-bottom-dcComics" href="">prova</a>
+                        <a class="links-bottom-dcComics" href="">prova</a>
+                        <a class="links-bottom-dcComics" href="">prova</a>
+                        <a class="links-bottom-dcComics" href="">prova</a>
+                        <a class="links-bottom-dcComics" href="">prova</a>
+                        <a class="links-bottom-dcComics" href="">prova</a>
+                        <a class="links-bottom-dcComics" href="">prova</a>
+                    </div>
+                </div>
+
+
+                <h3 id="shop-h3">SHOP</h3>
+
+                <div class="footer-links-shop">
+                    <a class="links-bottom-shop" href="">prova</a>
+                    <a class="links-bottom-shop" href="">prova</a>
+
+                </div>
+
+
+                <div id="dc">
+                    <h3 id="dc-h3">DC</h3>
+
+                    <div class="footer-links-dc">
+                        <a class="links-bottom-dc" href="">prova</a>
+                        <a class="links-bottom-dc" href="">prova</a>
+                        <a class="links-bottom-dc" href="">prova</a>
+                        <a class="links-bottom-dc" href="">prova</a>
+                        <a class="links-bottom-dc" href="">prova</a>
+                        <a class="links-bottom-dc" href="">prova</a>
+                        <a class="links-bottom-dc" href="">prova</a>
+                        <a class="links-bottom-dc" href="">prova</a>
+                        <a class="links-bottom-dc" href="">prova</a>
+                        <a class="links-bottom-dc" href="">prova</a>
+                        <a class="links-bottom-dc" href="">prova</a>
+                    </div>
+                </div>
+
+
+            </div>
+
+            <img id="logo-big-dc" src="dc-logo-bg.png" alt="" />
+
+            <div id="nav-button">
+                <button type="button">SIGN-UP NOW!</button>
+
+
+                <div class="navbar-bottom">
+                    <div id="follow">FOLLOW US</div>
+
+                    <img class="social" src="/public/footer-facebook.png" alt="" />
+                    <img class="social" src="/public/footer-twitter.png" alt="" />
+                    <img class="social" src="/public/footer-youtube.png" alt="" />
+                    <img class="social" src="/public/footer-pinterest.png" alt="" />
+                    <img class="social" src="/public/footer-periscope.png" alt="" />
+                </div>
+
+
+            </div>
+
+
+
+        </div>
+
+    )
+
+
+
+
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,33 @@
+function Header() {
+
+    return (
+
+        <div id="container">
+            <header>
+                <img id ="logo-piccolo" src="dc-logo.png" alt="Logo Navbar" />
+                <nav class="navbar-nav">
+
+
+
+                    <a class="nav-link-top" href="#">Characters</a>
+                    <a class="nav-link-top" href="#">Comics</a>
+                    <a class="nav-link-top" href="#">TV</a>
+                    <a class="nav-link-top" href="#">Games</a>
+                    <a class="nav-link-top" href="#">Collectibles</a>
+                    <a class="nav-link-top" href="#">Videos</a>
+                    <a class="nav-link-top" href="#">Fans</a>
+                    <a class="nav-link-top" href="#">News</a>
+                    <a class="nav-link-top" href="#">Shop</a>
+
+
+
+                </nav>
+            </header>
+        </div>
+
+    )
+
+
+}
+
+export default Header;
