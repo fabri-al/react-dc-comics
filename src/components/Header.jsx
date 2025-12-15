88a157d1dab358790d2ddc3ded1ca07233b8a83a
MyUsername <development.for.code@gmail.com>
2025-12-15 18:04:36 +0100

    add-import

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,33 @@
+import style from "../index.css";
+
+
 function Header() {
 
     return (
-
-        <div id="container">
-            <header>
-                <img id ="logo-piccolo" src="dc-logo.png" alt="Logo Navbar" />
-                <nav class="navbar-nav">
-
-
-
-                    <a class="nav-link-top" href="#">Characters</a>
-                    <a class="nav-link-top" href="#">Comics</a>
-                    <a class="nav-link-top" href="#">TV</a>
-                    <a class="nav-link-top" href="#">Games</a>
-                    <a class="nav-link-top" href="#">Collectibles</a>
-                    <a class="nav-link-top" href="#">Videos</a>
-                    <a class="nav-link-top" href="#">Fans</a>
-                    <a class="nav-link-top" href="#">News</a>
-                    <a class="nav-link-top" href="#">Shop</a>
+        <header className={style.header}>
+            <div id="container">
+                <div>
+                    <img id="logo-piccolo" src="dc-logo.png" alt="Logo Navbar" />
+                    <nav class="navbar-nav">
 
 
 
-                </nav>
-            </header>
-        </div>
+                        <a class="nav-link-top" href="#">Characters</a>
+                        <a class="nav-link-top" href="#">Comics</a>
+                        <a class="nav-link-top" href="#">TV</a>
+                        <a class="nav-link-top" href="#">Games</a>
+                        <a class="nav-link-top" href="#">Collectibles</a>
+                        <a class="nav-link-top" href="#">Videos</a>
+                        <a class="nav-link-top" href="#">Fans</a>
+                        <a class="nav-link-top" href="#">News</a>
+                        <a class="nav-link-top" href="#">Shop</a>
 
+
+
+                    </nav>
+                </div>
+            </div>
+        </header>
     )
 
 
