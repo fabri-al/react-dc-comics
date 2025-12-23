89455890fc9efadd4ea1f55749764c1f27f50054
MyUsername <development.for.code@gmail.com>
2025-12-23 17:52:42 +0100

    added-array-of-objects

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import Header from "./components/Header.jsx";
 import Main from "./components/Main.jsx";
 import Footer from "./components/Footer.jsx";
+
+
 function App() {
 
+   <a class="nav-link-top" href="#">Characters</a>,
+   <a class="nav-link-top" href="#">Comics</a>
 
   return (
     <>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,116 @@
-import style from "../index.css";
+import { useState } from "react";
 
+//Creo un array di oggetti
+const products = [ 
 
+    {
+        id: 1,              
+        testo: "Characters",
+        link: "#",
+        active: false        //per ogni oggetto, oltre alle altre chiavi, creo anche una chiave che sia booleana, 
+                            //per capire qual è l'oggetto attivo
+    },
+
+    {
+        id: 2,
+        testo: "Comics",
+        link: "#",
+        active: true
+    },
+
+    {
+        id: 3,
+        testo: "Movies",
+        link: "#",
+        active: false
+    },
+
+    {
+        id: 4,
+        testo: "tv",
+        link: "#",
+        active: false
+
+    },
+
+    {
+        id: 5,
+        testo: "Games",
+        link: "#",
+        active: false
+    },
+
+    {
+        id: 6,
+        testo: "collectibles",
+        link: "#",
+        active: false
+    },
+
+    {
+        id: 7,
+        testo: "videos",
+        link: "#",
+        active: false
+    },
+
+    {
+        id: 8,
+        testo: "fans",
+        link: "#",
+        active: false
+    },
+
+    {
+        id: 9,
+        testo: "news",
+        link: "#",
+        active: false
+    },
+
+    {
+        id: 10,
+        testo: "shop",
+        link: "#",
+        active: false
+    }
+
+]
+
+//Creo la funzione
 function Header() {
 
     return (
-        <header className={style.header}>
-            <div id="container">
-                <div>
-                    <img id="logo-piccolo" src="dc-logo.png" alt="Logo Navbar" />
-                    <nav class="navbar-nav">
 
+        <div id="container">
+            <header>
+                <img id="logo-piccolo" src="dc-logo.png" alt="Logo Navbar" />
+                <nav class="navbar-nav">
 
+                    {
+                        products.map((product, i) => { //con il metodo map prendo l'elemento e l'indice
 
-                        <a class="nav-link-top" href="#">Characters</a>
-                        <a class="nav-link-top" href="#">Comics</a>
-                        <a class="nav-link-top" href="#">TV</a>
-                        <a class="nav-link-top" href="#">Games</a>
-                        <a class="nav-link-top" href="#">Collectibles</a>
-                        <a class="nav-link-top" href="#">Videos</a>
-                        <a class="nav-link-top" href="#">Fans</a>
-                        <a class="nav-link-top" href="#">News</a>
-                        <a class="nav-link-top" href="#">Shop</a>
+                            let classeDaUsare = (product.active === true) ? "link-top" : ""; //creo una variabile d'appoggio per
+                                                                                            //l'operatore ternario
 
+                            /* let classeDaUsare = "";         
+                            if (product.active === true) {     //alternativa all'operatore ternario
+                                classeDaUsare = "link-top";
+                                }  */
+                          
 
+                            return <li key={i} className={classeDaUsare}>{product.testo}</li>
+                        })
 
-                    </nav>
-                </div>
-            </div>
-        </header>
+                    }
+
+                </nav>
+            </header>
+        </div>
+
     )
 
 
 }
-
-export default Header;
+//Esporto la funzione
+export default Header; 
